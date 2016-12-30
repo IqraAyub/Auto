b3cca048eacda2697f3704458b9da3b35a4b30fc
iayub <iayub@xelleration.com>
2016-12-30 17:20:51 +0500

    initial commit

---
+++ AutomaticDashroad/AutomaticDashroad/app/app.js
@@ -0,0 +1,41 @@
+﻿var myApp = angular.module('myApp', []);
+//myApp.config(function ($httpProvider, $routeProvider) {
+//    $routeProvider
+
+//        .when('/', {
+//            templateUrl: 'index.html',
+//            controller: 'myCtrl'
+//        });
+//});
+
+
+//myApp.factory('datacontext', datacontext);
+//datacontext.$inject = ['$scope', '$http'];
+//myApp.factory('dataFactory', ['$scope', '$http', function dataFactory($scope, $http) {
+//    var service = {
+//        get: get
+//    };
+
+//    var baseUrl = "https://api.automatic.com/vehicle/";
+//    return service;
+
+//    var config = {
+//        headers: {
+//            'Authorization': 'Bearer e39bdae812a9c2c6d8e3b37a190f9e6445e8df1a'
+//        }
+//    }
+//    function get() {
+
+//        $http.get(baseUrl, config).success(function (response) {
+//            $scope.data = response;
+//            alert("im here in datacontext");
+//            console.log($scope.data);
+//        });
+//    }
+
+//}]);
+
+
+
+
+
---
+++ AutomaticDashroad/AutomaticDashroad/app/myCtrl.js
@@ -0,0 +1,109 @@
+﻿myApp.controller('myCtrl', ['$scope','$http', function ($scope, $http) {
+
+    $scope.veh = { make: 'Lexus', model: 'ES 350', year: '2016', bodyType: 'Sedan', fuelType: 'Gasoline', powerTrain: 'Front wheel drive', vinId: 'JTHBK1GG9G2219733' };
+
+    var baseUrl = "https://api.automatic.com/vehicle/";
+    var config = {
+        headers: {
+            'Authorization': 'Bearer e39bdae812a9c2c6d8e3b37a190f9e6445e8df1a'
+        }
+    }
+    $http.get(baseUrl, config).success(function (response) {
+        var data = response;
+        console.log(data);
+    });
+    //Vehicle Score
+    var ctx = document.getElementById("scroe");
+    var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: [
+                "Best Condition",
+                "White"
+            ],
+            datasets: [
+                {
+                    data: [100, 50],
+                    backgroundColor: [
+                        "#90EE90",
+                        "#FFFAF0"
+                    ],
+                    hoverBackgroundColor: [
+                        "#90EE90",
+                        "#FFFAF0"
+                    ]
+                }]
+        },
+        options: {
+            elements: {
+                arc: {
+                    borderColor: "#FFFAF0"
+                }
+            }
+        }
+    });
+    //% Heavy Driving
+    var ctx = document.getElementById("driving");
+    var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: [
+                "Heavy Drive",
+                "Normal Drive"
+            ],
+            datasets: [
+                {
+                    data: [40, 60],
+                    backgroundColor: [
+                        "#DC143C",
+                        "#90EE90"
+                    ],
+                    hoverBackgroundColor: [
+                        "#DC143C",
+                        "#90EE90"
+                    ]
+                }]
+        },
+        chart: {
+            defaultCenterLabel: "Total revenue: $60K",
+            centerLabel: "Revenue from"
+        },
+        options: {
+            elements: {
+                arc: {
+                    borderColor: "#FFFAF0"
+                }
+            }
+        }
+    });
+    //Highway VS City
+    var ctx = document.getElementById("highwayVScity");
+    var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: [
+                "Highway 60%",
+                "City 40%"
+            ],
+            datasets: [
+                {
+                    data: [100, 50],
+                    backgroundColor: [
+                        "#FF6384",
+                        "#36A2EB"
+                    ],
+                    hoverBackgroundColor: [
+                        "#FF6384",
+                        "#36A2EB"
+                    ]
+                }]
+        },
+        options: {
+            elements: {
+                arc: {
+                    borderColor: "#FFFAF0"
+                }
+            }
+        }
+    });
+}]);
